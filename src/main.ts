1a2bc0e2ab0f2a4d5c47b2c3c378b267fa418d04
yacine18 <hilali.yacin@gmail.com>
2022-06-13 17:54:54 +0100

    deploy to heroku

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
   app.enableCors()
-  await app.listen(8800);
+  await app.listen(process.env.PORT || 8800);
 }
 bootstrap();
